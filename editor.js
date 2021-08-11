501dcfad80e9149bcba8e67d13400047a5c20a73
prasantdev <prasantsahoorebel@gmail.com>
2021-08-11 19:01:27 +0530

    fix size

--- editor.js
+++ editor.js
@@ -1,12 +1,12 @@
 //getting elements
 const editorHeaders = document.getElementById('editor-header-container');
 const workspaceContainer = document.getElementById('workspace-container');
+const inpKey = document.getElementById('input-key');
 
+//Event Listeners
 editorHeaders.addEventListener('click', (e) => {
     // console.log(e.target)
     if (e.target.className.includes('mode-open')) return;
-    // let openingSpace = e.target.dataset.openmode;
-    // console.log(openingSpace)
 
     editorHeaders.childNodes.forEach(elem => {
         if (elem.className && elem.className.includes('mode-open')) {
@@ -15,14 +15,16 @@ editorHeaders.addEventListener('click', (e) => {
     })
 
     workspaceContainer.childNodes.forEach(el => {
-            if (el.id) {
-                if (el.id === e.target.dataset.openmode) {
-                    el.className.includes('workspace-closed') ? el.classList.remove('workspace-closed') : false;
-                } else {
-                    el.classList.add('workspace-closed');
-                }
+        if (el.id) {
+            if (el.id === e.target.dataset.openmode) {
+                el.className.includes('workspace-closed') ? el.classList.remove('workspace-closed') : false;
+            } else {
+                el.classList.add('workspace-closed');
             }
-        })
-        // console.log(workspaceContainer.childNodes)
+        }
+    });
+    // console.log(workspaceContainer.childNodes)
     e.target.classList.add('mode-open');
-})
+});
+
+inpKey.addEventListener('input')
