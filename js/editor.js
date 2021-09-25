9904b0fd98695c2fc5e8db69bbea180629199e82
prasantdev <prasantsahoorebel@gmail.com>
2021-09-25 09:08:42 +0530

    fix json editor issues

--- js/editor.js
+++ js/editor.js
@@ -6,6 +6,7 @@ const preview = document.getElementById('preview-body');
 const inpValue = document.getElementById('input-value');
 const addKey = document.getElementById('input-key-add');
 const cancelKey = document.getElementById('input-key-cancel');
+const deleteKey = document.getElementById('delete-key');
 const previewPre = document.getElementById('pre-code');
 const previewCode = document.getElementById('code-code');
 
@@ -34,28 +35,42 @@ editorHeaders.addEventListener('click', (e) => {
 //Functions
 let obj = {};
 function addInpKey() {
-let k = inpKey.value;
-let vTemp = inpValue.value;
-// let v = typeof JSON.parse(vTemp) === 'object'?typeof JSON.parse(vTemp) === 'object':(Number(vTemp)?Number(vTemp):vTemp);
-let v;
-if(isParsable(vTemp) && typeof JSON.parse(vTemp) === 'object'){
-    console.log('yes')
-    v = JSON.parse(vTemp);
-}else if(Number(vTemp)){
-    v = Number(vTemp);
-}else{ 
-    v = vTemp;
-}
-obj[k] = v;
-let jsonCode = JSON.stringify(obj, undefined, 2).replace(/</g, '&lt;').replace(/&/g, '&amp;');
-previewCode.innerHTML = jsonCode;
-Prism.highlightElement(previewCode);
-// previewPre.setAttribute('data-src', `javascript:downloadFile('f${new Date().getTime().toString()}.json,  ${JSON.stringify(obj)}`);
+    let k = inpKey.value;
+    let vTemp = inpValue.value;
+    // let v = typeof JSON.parse(vTemp) === 'object'?typeof JSON.parse(vTemp) === 'object':(Number(vTemp)?Number(vTemp):vTemp);
+    let v;
+    if (isParsable(vTemp) && typeof JSON.parse(vTemp) === 'object') {
+        console.log('yes')
+        v = JSON.parse(vTemp);
+    } else if (Number(vTemp)) {
+        v = Number(vTemp);
+    } else {
+        v = vTemp;
+    }
+    obj[k] = v;
+    let jsonCode = JSON.stringify(obj, undefined, 2).replace(/</g, '&lt;').replace(/&/g, '&amp;');
+    previewCode.innerHTML = jsonCode;
+    Prism.highlightElement(previewCode);
+    // previewPre.setAttribute('data-src', `javascript:downloadFile('f${new Date().getTime().toString()}.json,  ${JSON.stringify(obj)}`);
 }
 
-addKey.addEventListener('click', ()=> {
+addKey.addEventListener('click', () => {
     addInpKey();
 })
+deleteKey.addEventListener('click', () => {
+    let k = inpKey.value;
+    delete obj[k];
+    let jsonCode = JSON.stringify(obj, undefined, 2).replace(/</g, '&lt;').replace(/&/g, '&amp;');
+    previewCode.innerHTML = jsonCode;
+    Prism.highlightElement(previewCode);
+})
+
+function createNew() {
+    inpKey.value = inpValue.value = '';
+    obj = {};
+    previewCode.innerHTML = '';
+
+}
 
 //lib 
 function isParsable(str) {
@@ -71,11 +86,11 @@ function downloadFile(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
-  
+
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     document.body.removeChild(element);
-  }
+}
